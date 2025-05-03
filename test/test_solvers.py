d1891f04f61ecc49560d44656a298b9c201882c6
Noir <60018820+sZeta99@users.noreply.github.com>
2025-05-03 14:11:41 +0200

    Test Solver update

--- test/test_solvers.py
+++ test/test_solvers.py
@@ -1,15 +1,45 @@
+from typing import Any, Final, cast
+from abc import ABC, abstractmethod
 import pytest
+import scipy.io
 import numpy as np
-from solvers import JacobiSolver
+from solvers import IterativeSolver, JacobiSolver, GaussSeidelSolver, GradientDescentSolver, ConjugateGradientSolver
 
-class TestJacobiSolver:
+class AbstractIterativeSolverTest(ABC):
+  @abstractmethod
+  def provide(self) -> IterativeSolver:
+    pass
+
   def test_solve(self):
-    solver = JacobiSolver()
-    A = np.eye(3, dtype=float)
-    b = np.array([1.0, 2.0, 3.0])
-    x, info = solver.solve(A, b)
-    assert np.allclose(x, b)
-    assert info is not None
+    solver: Final[IterativeSolver] = self.provide()
+    for data in ["spa1", "spa2", "vem1", "vem2"]:
+      with open(f"./data/{data}.mtx", encoding="ascii") as mtx:
+        A = cast(np.typing.NDArray[Any], scipy.io.mmread(mtx).toarray())
+        x = np.ones(A.shape[1])
+        b = A @ x
+        x_apx, info = solver.solve(A, b)
+        rel_err = np.linalg.norm(x_apx - x) / np.linalg.norm(x)
+        assert np.allclose(x_apx, x, atol=5e-2)
+        assert rel_err < 1e-3
+        assert info.converged
+        assert info.iterations < 20_000
+        assert info.residual < 1e-6
+
+class TestJacobiSolver(AbstractIterativeSolverTest):
+  def provide(self) -> IterativeSolver:
+    return JacobiSolver(1e-6)
+
+class TestGaussSeidelSolver(AbstractIterativeSolverTest):
+  def provide(self) -> IterativeSolver:
+    return GaussSeidelSolver(1e-6)
+
+class TestGradientDescentSolver(AbstractIterativeSolverTest):
+  def provide(self) -> IterativeSolver:
+    return GradientDescentSolver(1e-6)
+
+class TestConjugateGradientSolver(AbstractIterativeSolverTest):
+  def provide(self) -> IterativeSolver:
+    return ConjugateGradientSolver(1e-6)
 
 if __name__ == "__main__":
   pytest.main()
